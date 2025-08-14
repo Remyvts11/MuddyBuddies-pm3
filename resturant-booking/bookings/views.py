3447cdd7d800d6ca91a67ff07e5131574319f5bf
Remyvts11 <vrjs6kz6@students.codeinstitute.net>
2025-08-14 21:30:37 +0000

    added CRUD operations

--- resturant-booking/bookings/views.py
+++ resturant-booking/bookings/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here.from django.shortcuts import render, redirect, get_object_or_404
+from .models import Booking
+from .forms import BookingForm
+
+def booking_list(request):
+    bookings = Booking.objects.order_by('-date','-time')
+    return render(request, 'bookings/booking_list.html', {'bookings': bookings})
+
+def booking_create(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('booking_list')
+    else:
+        form = BookingForm()
+    return render(request, 'bookings/booking_form.html', {'form': form})
+
+def booking_update(request, pk):
+    booking = get_object_or_404(Booking, pk=pk)
+    if request.method == 'POST':
+        form = BookingForm(request.POST, instance=booking)
+        if form.is_valid():
+            form.save()
+            return redirect('booking_list')
+    else:
+        form = BookingForm(instance=booking)
+    return render(request, 'bookings/booking_form.html', {'form': form})
+
+def booking_delete(request, pk):
+    booking = get_object_or_404(Booking, pk=pk)
+    if request.method == 'POST':
+        booking.delete()
+        return redirect('booking_list')
+    return render(request, 'bookings/booking_confirm_delete.html', {'booking': booking})
